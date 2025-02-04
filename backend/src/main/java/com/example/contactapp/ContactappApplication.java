a359cc40ddc50d72d74c52b10387536468d16d49
TobyQuality <topias.laatu@gmail.com>
2025-02-04 16:49:46 +0200

    Finalization for push to remote repository

--- backend/src/main/java/com/example/contactapp/ContactappApplication.java
+++ backend/src/main/java/com/example/contactapp/ContactappApplication.java
@@ -2,10 +2,16 @@ package com.example.contactapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ContactappApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load(); // Load .env file
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        
         SpringApplication.run(ContactappApplication.class, args);
     }
 }
