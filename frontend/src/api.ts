12d8175e236a1c0d769116ec43fd084b54a6fd67
TobyQuality <topias.laatu@gmail.com>
2025-02-07 18:35:02 +0200

    Change api url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/contacts";
+const API_URL = "https://contactapp-springboot.onrender.com/";
 
 export interface Contact {
   id?: number;
