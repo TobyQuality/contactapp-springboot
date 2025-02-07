e57f55aa85eacdbd52575c6c8e5d4556028fb673
TobyQuality <topias.laatu@gmail.com>
2025-02-07 18:57:47 +0200

    fix the url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://contactapp-springboot.onrender.com/";
+const API_URL = "https://contactapp-springboot.onrender.com/contacts";
 
 export interface Contact {
   id?: number;
